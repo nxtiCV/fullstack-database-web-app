1039bd2dcdb10cd635493e0ee50fa572cfa3b984
nxtiCV <natalie.venkatasami@gmail.com>
2026-04-29 19:16:44 -0400

    Update db.py

--- Database Project/Website/pycharm/db.py
+++ Database Project/Website/pycharm/db.py
@@ -3,7 +3,7 @@ import mysql.connector
 def get_connection():
     connection = mysql.connector.connect(
         user='root', # your username goes here, which is 'root'
-        password='soccerMan1#', #your password goes here
+        password='password#', #your password goes here
         host='localhost',
         database='car_rental'
     )
